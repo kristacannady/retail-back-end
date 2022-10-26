0cbcfd4ba81f2943fae26644430432913f6f7550
Krista Cannady <kristalcannady@gmail.com>
2022-10-26 15:06:16 -0400

    comments added

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
+//create producttag model 
 class ProductTag extends Model {}
 
 ProductTag.init(
--- models/Tag.js
+++ models/Tag.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+// create tag model 
 class Tag extends Model {}
 
 //define columns 
