d6cf6719c8560221c5677a2ae9f07f4a8f2dd4f7
Krista Cannady <kristalcannady@gmail.com>
2022-10-26 14:58:26 -0400

    defined columns

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 
+
+// connection to jawsdb and our db 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
